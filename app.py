ecd1dd52f9f644395d3119d0c0d4e3b2ac6ba66c
Jonaslima07 <jonaslima23122003@gmail.com>
2025-08-12 19:35:39 -0300

    feat:Final

--- app.py
+++ app.py
@@ -1,36 +1,4 @@
 import os
-<<<<<<< HEAD
-from flask import Flask, jsonify
-from flask_caching import Cache
-from sqlalchemy import text
-from helpers.database import init_db, db
-from models.cbo_sinonimos import Cbo_Sinonimos
-
-app = Flask(__name__)
-init_db(app)
-
-cache = Cache(config={
-    "CACHE_TYPE": "redis",
-    "CACHE_REDIS_HOST": os.getenv("REDIS_HOST", "localhost"),
-    "CACHE_REDIS_PORT": os.getenv("REDIS_PORT", 6379),
-    "CACHE_DEFAULT_TIMEOUT": 120
-})
-cache.init_app(app)
-
-@app.route("/cbo/<codigo>")
-@cache.cached()
-def get_cbo(codigo):
-    sql = text("SELECT * FROM cbo_sinonimos WHERE codigo = :codigo")
-    result = db.session.execute(sql, {"codigo": codigo}).mappings().all()
-
-    if not result:
-        return jsonify({"erro": "Código não encontrado"}), 404
-
-    return jsonify([dict(row) for row in result])
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
-=======
 import csv
 from flask import Flask, jsonify
 import psycopg2
@@ -111,4 +79,3 @@ def get_dado(codigo):
 if __name__ == "__main__":
     importar_csv()
     app.run(host="0.0.0.0", port=5000)
->>>>>>> ddbb873 (feat: ajustes finais)
